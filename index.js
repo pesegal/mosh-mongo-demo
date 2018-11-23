aebabc2e461c8ab2d75edd58b7b8756c5c49635f
Peter Segal <psegal@zoomcare.com>
2018-11-23 15:58:16 -0800

    add first query

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     // Create a new Course object
     const course = new Course({
-        name: 'Node.js Course',
+        name: 'Angular Course',
         author: 'Mosh',
-        tags: ['node', 'backend'],
+        tags: ['angular', 'frontend'],
         isPublished: true
     });
 
@@ -30,4 +30,13 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1});
+    console.log(courses);
+}
+
+getCourses();

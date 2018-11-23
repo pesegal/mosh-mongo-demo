6d6c878ef0f70fb74ff80e9f328646e8a9f6b54e
Peter Segal <psegal@zoomcare.com>
2018-11-23 15:36:35 -0800

    add courseSchema

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error("Could not connect to mongoDB."));
 
+// Defines the shape of the data (specific to moongoose, not mongoDB)
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ], 
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});

c9b1973f93d90feb9fb14eabd1c93c96f2d8a544
Peter Segal <psegal@zoomcare.com>
2018-11-23 16:08:47 -0800

    add logical operator example

--- index.js
+++ index.js
@@ -41,12 +41,19 @@ async function getCourses() {
     // in
     // nin (not in)
 
+    // MongoDB: Logical Operators
+    // or
+    // and
 
     const courses = await Course
-        .find({ author: 'Mosh', isPublished: true })
-        // Example of comparison operators
+        //.find({ author: 'Mosh', isPublished: true })
+        // --> Example of comparison operators
         //.find({ price: { $gte: 10, $lte: 20 }}) 
         //.find({ price: { $in: [10, 15, 20] }})
+        // --> Example of logical operators
+        // .find()
+        // .or([ {author: 'Mosh' }, { isPublished: true } ])
+        // .and([])
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1});

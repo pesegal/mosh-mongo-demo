eff2797c4bb43dbd1beac13949f2c7275a121115
Peter Segal <psegal@zoomcare.com>
2018-11-23 17:50:17 -0800

    add delete object function

--- index.js
+++ index.js
@@ -83,4 +83,9 @@ async function updateCourse(id) {
     console.log(result);
 }
 
-updateCourse('5bf8919c2c2958613cbcd243');
+async function removeCourse(id) {
+    const result = await Course.deleteOne( { _id: id });
+    console.log(result);
+}
+
+removeCourse('5bf8919c2c2958613cbcd243');
